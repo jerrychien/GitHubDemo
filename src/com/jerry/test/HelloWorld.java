c757c2c1b8d030146cdbf1bbd2116399b2b0fd19
jerrychien <qianzhiyong111@163.com>
2016-04-15 17:20:47 +0800

    first github commit

--- src/com/jerry/test/HelloWorld.java
+++ src/com/jerry/test/HelloWorld.java
@@ -11,5 +11,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("[jerry's log]-------------args's val() = [" + args + "]," + " current_method = HelloWorld.main()");
         System.out.println("hehe");
+        System.out.println("[jerry's log]-------------args's val() = [" + args + "]," + " current_method = HelloWorld.main()");
     }
 }
